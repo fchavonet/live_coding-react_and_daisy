14fc61c22ee1607f73eccee60ab84924478a66e1
fchavonet <fchavonet@mail.fr>
2025-11-06 08:32:13 +0100

    Create Programs section component.

--- src/components/Campuses.jsx
+++ src/components/Campuses.jsx
@@ -2,7 +2,7 @@ function Campuses() {
   return (
     <section id="campuses-section" className="w-full py-16 px-4 lg:py-24">
       <div className="max-w-6xl mx-auto flex flex-col justify-center items-center gap-24">
-        <h2 className="text-4xl font-semibold text-center">Campuses</h2>
+        <h2 className="text-6xl font-semibold text-center">Campuses</h2>
       </div>
     </section>
   );
--- src/components/Methodology.jsx
+++ src/components/Methodology.jsx
@@ -2,7 +2,7 @@ function Methodology() {
   return (
     <section id="methodology-section" className="w-full py-16 px-4 lg:py-24">
       <div className="max-w-6xl mx-auto flex flex-col justify-center items-center gap-24">
-        <h2 className="text-4xl font-semibold text-center">Methodology</h2>
+        <h2 className="text-6xl font-semibold text-center">Methodology</h2>
 
         <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
           <li>
